f9fdcfbc279ff9a58d9d7804435c10526d6b94e3
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2020-11-08 23:42:51 -0500

    Added tests for `codepost_stats.helpers`

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -4,6 +4,12 @@ import pytest
 import codepost_stats.helpers
 
 
+SOME_NEGATIVE_NUMBER = -10
+
+
+def test_check_int_like_none():
+    assert codepost_stats.helpers.check_int_like(None) is None
+
 @pytest.mark.parametrize(
     "input, output", [
         (1, 1),
@@ -24,3 +30,6 @@ def test_check_int_like_success(input, output):
 def test_check_int_like_fail(input):
     with pytest.raises(TypeError):
         ret = codepost_stats.helpers.check_int_like(input)
+
+def test_check_int_like_negative():
+    assert codepost_stats.helpers.check_int_like(SOME_NEGATIVE_NUMBER) is None
